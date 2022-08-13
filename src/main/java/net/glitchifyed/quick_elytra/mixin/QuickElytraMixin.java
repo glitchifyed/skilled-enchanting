7bb45f3bf5d40b16f8480b52634852548af27885
glitchifyed <allglitched@gmail.com>
2022-08-14 11:27:34 +1200

    first commit

--- src/main/java/net/glitchifyed/quick_elytra/mixin/QuickElytraMixin.java
+++ src/main/java/net/glitchifyed/quick_elytra/mixin/QuickElytraMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.glitchifyed.quick_elytra.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.glitchifyed.quick_elytra.QuickElytra;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class QuickElytraMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		QuickElytra.LOGGER.info("This line is printed by a QuickElytra mixin!");
 	}
 }
