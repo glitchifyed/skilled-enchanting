1227ff640ca7fc68fe63f449ba3c15ebe6d6717d
glitchifyed <allglitched@gmail.com>
2022-08-14 11:48:40 +1200

    toggle equip keybind added. sends a chat message

---
+++ src/main/java/net/glitchifyed/quick_elytra/QuickElytraClient.java
@@ -0,0 +1,19 @@
+package net.glitchifyed.quick_elytra;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.glitchifyed.quick_elytra.event.KeyInputHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuickElytraClient implements ClientModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitializeClient() {
+		KeyInputHandler.register();
+	}
+}
--- src/main/java/net/glitchifyed/quick_elytra/event/KeyInputHandler.java
+++ src/main/java/net/glitchifyed/quick_elytra/event/KeyInputHandler.java
@@ -1,5 +1,34 @@
 package net.glitchifyed.quick_elytra.event;
 
-public class KeyInputHandler {
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
+public class KeyInputHandler {
+    public static final String KEY_CATEGORY_EQUIP = "key.category.glitchifyed.quick_elytra";
+    public static final String KEY_TOGGLE_EQUIP = "key.glitchifyed.quick_elytra.toggle_equip";
+
+    public static KeyBinding toggleEquip;
+
+    public static void registerKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (toggleEquip.wasPressed()) {
+                client.player.sendChatMessage("TOGGLE EQUIP PRESSED", Text.literal("your amongus"));
+            }
+        });
+    }
+
+    public static void register() {
+        toggleEquip = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_TOGGLE_EQUIP,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                KEY_CATEGORY_EQUIP
+        ));
+
+        registerKeyInputs();
+    }
 }
