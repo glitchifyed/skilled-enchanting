c549a2497a4e3425800f22ba2d9dc696a453d14f
glitchifyed <allglitched@gmail.com>
2022-08-15 11:31:23 +1200

    v1.0.0 release

--- src/main/java/net/glitchifyed/quick_elytra/event/KeyInputHandler.java
+++ src/main/java/net/glitchifyed/quick_elytra/event/KeyInputHandler.java
@@ -2,9 +2,17 @@ package net.glitchifyed.quick_elytra.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.glitchifyed.quick_elytra.QuickElytra;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.collection.DefaultedList;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
@@ -13,10 +21,78 @@ public class KeyInputHandler {
 
     public static KeyBinding toggleEquip;
 
+    final static int ARMOUR_SLOT = 6;
+    static boolean togglePressed;
+
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (toggleEquip.wasPressed()) {
-                client.player.sendChatMessage("TOGGLE EQUIP PRESSED", Text.literal("your amongus"));
+            boolean togglePress = toggleEquip.isPressed();
+
+            if (togglePress && !togglePressed) {
+                togglePressed = true;
+
+                MinecraftClient CLIENT = MinecraftClient.getInstance();
+
+                int swapSlot = -1;
+
+                DefaultedList<ItemStack> inventory = CLIENT.player.getInventory().main;
+
+                ItemStack chestplate = CLIENT.player.getInventory().armor.get(2);
+                boolean notWearing = chestplate.getItem() == Items.AIR;
+                boolean wearingChestplate = isItemChestplate(chestplate);
+                boolean wearingElytra = isItemElytra(chestplate);
+
+                int highEnchantLevel = -1;
+                int highestDurability = -1;
+
+                for (int i = 0; i < inventory.size(); i++) {
+                    ItemStack itemStack = inventory.get(i);
+                    Item item = itemStack.getItem();
+
+                    if (item == Items.AIR) {
+                        continue;
+                    }
+
+                    if (notWearing || wearingChestplate && isItemElytra(itemStack) || wearingElytra && isItemChestplate(itemStack)) {
+                        int durability = itemStack.getMaxDamage();
+
+                        if (durability > highestDurability) {
+                            highestDurability = durability;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < inventory.size(); i++) {
+                    ItemStack itemStack = inventory.get(i);
+                    Item item = itemStack.getItem();
+
+                    if (item == Items.AIR) {
+                        continue;
+                    }
+
+                    if (notWearing || wearingChestplate && isItemElytra(itemStack) || wearingElytra && isItemChestplate(itemStack)) {
+                        int level = getEnchantLevel(itemStack);
+                        int durability = itemStack.getMaxDamage();
+
+                        if (durability == highestDurability && level > highEnchantLevel) {
+                            swapSlot = i;
+                            highEnchantLevel = level;
+                        }
+                    }
+                }
+
+                if (swapSlot != -1) {
+                    CLIENT.interactionManager.clickSlot(
+                            CLIENT.player.playerScreenHandler.syncId,
+                            ARMOUR_SLOT,
+                            swapSlot,
+                            SlotActionType.SWAP,
+                            CLIENT.player
+                    );
+                }
+            }
+            else if (!togglePress && togglePressed) {
+                togglePressed = false;
             }
         });
     }
@@ -30,5 +106,40 @@ public class KeyInputHandler {
         ));
 
         registerKeyInputs();
+    }
+
+    static int getEnchantLevel(ItemStack itemStack) {
+        int level = 0;
+        NbtList enchants = itemStack.getEnchantments();
+
+        if (enchants.size() > 0) {
+            for (int i = 0; i < enchants.size(); i++) {
+                NbtElement nbtEnchant = enchants.get(i);
+                String strEnchant = nbtEnchant.asString();
+
+                String lvlSeparate = strEnchant.replaceAll("\\D+", "");
+
+                level += Integer.parseInt(lvlSeparate);
+            }
+        }
+
+        return level;
+    }
+
+    static boolean isItemEquipChestplate(ItemStack itemStack) {
+        return isItemElytra(itemStack) && isItemChestplate(itemStack);
+    }
+
+    static boolean isItemChestplate(ItemStack itemStack) {
+        Item item = itemStack.getItem();
+        String itemName = item.getName().getString();
+
+        return MobEntity.getPreferredEquipmentSlot(itemStack).getType() == EquipmentSlot.Type.ARMOR && itemName.toLowerCase().contains("chestplate");
+    }
+
+    static boolean isItemElytra(ItemStack itemStack) {
+        Item item = itemStack.getItem();
+
+        return item == Items.ELYTRA;
     }
 }
